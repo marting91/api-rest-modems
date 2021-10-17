c647f8578053a25854f394db2404d8f603800fbd
Martin Gonzalez <gonzalez.martin.mag@gmail.com>
2021-10-17 19:35:25 -0300

    Sweet alert para los mensajes de error

--- public/js/app.js
+++ public/js/app.js
@@ -27,6 +27,12 @@ async function addModel(e) {
                 showConfirmButton: false,
                 timer: 1500
             });
+        } else {
+            Swal.fire({
+                icon: 'error',
+                title: 'Ups!',
+                text: result.msg,
+            })
         }
     };
 }
@@ -35,28 +41,49 @@ async function getModems(e) {
     tableBody.innerHTML = '';
     const vendorSelected = e.target.value;
     const modems = await callApi('GET', `/fabricantes/${vendorSelected}/modems`);
-    for (const modem of modems) {
-        const row = document.createElement('tr');
-        row.innerHTML = `
-            <td data-modem="macaddr">${modem.modem_macaddr}</td>
-            <td data-modem="ipaddr">${modem.ipaddr}</td>
-            <td data-modem="vsi_model">${modem.vsi_model}</td>
-            <td data-modem="vsi_swver">${modem.vsi_swver}</td>
-            <td class="text-center"><svg role="button" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M12 22C6.47967 21.994 2.00606 17.5204 2 12V11.8C2.10993 6.30455 6.63459 1.92797 12.1307 2.0009C17.6268 2.07382 22.0337 6.5689 21.9978 12.0654C21.9619 17.5618 17.4966 21.9989 12 22ZM7 11V13H11V17H13V13H17V11H13V7.00002H11V11H7Z" fill="#2E3A59"></path>
-            </svg>
-            </td>
-        `
-        tableBody.appendChild(row);
+
+    if (!modems.msg) {
+        for (const modem of modems) {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td data-modem="macaddr">${modem.modem_macaddr}</td>
+                <td data-modem="ipaddr">${modem.ipaddr}</td>
+                <td data-modem="vsi_model">${modem.vsi_model}</td>
+                <td data-modem="vsi_swver">${modem.vsi_swver}</td>
+                <td class="text-center"><svg role="button" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M12 22C6.47967 21.994 2.00606 17.5204 2 12V11.8C2.10993 6.30455 6.63459 1.92797 12.1307 2.0009C17.6268 2.07382 22.0337 6.5689 21.9978 12.0654C21.9619 17.5618 17.4966 21.9989 12 22ZM7 11V13H11V17H13V13H17V11H13V7.00002H11V11H7Z" fill="#2E3A59"></path>
+                </svg>
+                </td>
+            `
+            tableBody.appendChild(row);
+        }
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Ups!',
+            text: modems.msg,
+        })
     }
+
+
 }
 
 async function loadVendors() {
     const vendors = await callApi('GET', '/fabricantes');
-    for (const vendor of vendors) {
-        const option = document.createElement('option');
-        option.textContent = vendor.vsi_vendor;
-        option.value = vendor.vsi_vendor;
-        vendorSelect.appendChild(option);
+
+    if (!vendors.msg) {
+        for (const vendor of vendors) {
+            const option = document.createElement('option');
+            option.textContent = vendor.vsi_vendor;
+            option.value = vendor.vsi_vendor;
+            vendorSelect.appendChild(option);
+        }
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Ups!',
+            text: vendors.msg,
+        })
     }
+
 }
